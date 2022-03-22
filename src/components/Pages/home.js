159a99d08cfce202dbc389e0d92e2f649ddc1d6d
Marc raphael <marcraphael979@gmail.com>
2022-03-22 14:53:56 +0100

    components/Pages/home: create the Home component

---
+++ src/components/Pages/home.js
@@ -0,0 +1,12 @@
+export default function Home () {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vel consectetur interdum, nisl nisi tincidunt nisi, eget egestas nisl nisi euismod nisi. Nullam euismod, nisi vel consectetur interdum, nisl nisi tincidunt nisi, eget egestas nisl nisi euismod nisi. Nullam euismod, nisi vel consectetur interdum, nisl nisi tincidunt nisi, eget egestas nisl nisi euismod nisi. Nullam euismod, nisi vel consectetur interdum, nisl nisi tincidunt nisi, eget egestas nisl nisi euismod nisi. Nullam euismod, nisi vel consectetur interdum,
+      </p>
+      <p>
+        nisl nisi tincidunt nisi, eget egestas nisl nisi euismod nisi. Nullam euismod, nisi vel consectetur interdum, nisl nisi tincidunt nisi, eget egestas nisl nisi euismod nisi. Nullam euismod, nisi vel consectetur interdum, nisl nisi tincidunt nisi, eget egestas nisl nisi euismod nisi. Nullam euismod, nisi vel consectetur interdum, nisl nisi tincidunt nisi, eget egestas nisl nisi euismod nisi. Nullam euismod, nisi vel consectetur interdum, nisl nisi tincidunt nisi, eget egestas nisl nisi euismod nisi. Nullam euismod, nisi vel consectetur interdum, nisl nisi tincidunt nisi, eget egestas nisl nisi euismod nisi. Nullam euismod, nisi vel consectetur interdum, nisl nisi tincidunt nisi, eget
+      </p>
+    </div>
+  )
+}
