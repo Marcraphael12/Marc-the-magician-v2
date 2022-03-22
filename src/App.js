e33f4091b0d17dac42c83133cd8015155eadec84
Marc raphael <marcraphael979@gmail.com>
2022-03-22 14:53:31 +0100

    App: Udate imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 // Components list
 import Calculator from './components/calculator';
