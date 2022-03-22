a5dc5a8860d04a3e5200cf372c18019fcb1d9008
Marc raphael <marcraphael979@gmail.com>
2022-03-22 14:37:36 +0100

    App:  Implement Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+
 import Calculator from './components/calculator';
+
 
 const App = () => {
   return (
     <>
-      <Calculator />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="calculator" element={<Calculator />} />
+        <Route path="quote" element={<Quote />} />
+      </Routes>
     </>
   );
 }
