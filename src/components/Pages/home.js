0ceea9bd0e6a5dacdc35e86f2da8cfd542dc168f
Marc raphael <marcraphael979@gmail.com>
2022-03-22 20:13:18 +0100

    Add new element blocks

--- src/components/Pages/home.js
+++ src/components/Pages/home.js
@@ -3,12 +3,13 @@ import './home.css';
 export default function Home () {
   return (
     <div className='Home'>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vel consectetur interdum, nisl nisi tincidunt nisi, eget egestas nisl nisi euismod nisi. Nullam euismod, nisi vel consectetur interdum, nisl nisi tincidunt nisi, eget egestas nisl nisi euismod nisi. Nullam euismod, nisi vel consectetur interdum, nisl nisi tincidunt nisi, eget egestas nisl nisi euismod nisi. Nullam euismod, nisi vel consectetur interdum, nisl nisi tincidunt nisi, eget egestas nisl nisi euismod nisi. Nullam euismod, nisi vel consectetur interdum,
-      </p>
-      <p>
-        nisl nisi tincidunt nisi, eget egestas nisl nisi euismod nisi. Nullam euismod, nisi vel consectetur interdum, nisl nisi tincidunt nisi, eget egestas nisl nisi euismod nisi. Nullam euismod, nisi vel consectetur interdum, nisl nisi tincidunt nisi, eget egestas nisl nisi euismod nisi. Nullam euismod, nisi vel consectetur interdum, nisl nisi tincidunt nisi, eget egestas nisl nisi euismod nisi. Nullam euismod, nisi vel consectetur interdum, nisl nisi tincidunt nisi, eget egestas nisl nisi euismod nisi. Nullam euismod, nisi vel consectetur interdum, nisl nisi tincidunt nisi, eget egestas nisl nisi euismod nisi. Nullam euismod, nisi vel consectetur interdum, nisl nisi tincidunt nisi, eget
-      </p>
+      <h1>Welcome to The Math Magic App</h1>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vel consectetur interdum, nisl nisi tincidunt nisi, eget egestas nisl nisi euismod nisi. Nullam euismod, nisi vel consectetur interdum, nisl nisi tincidunt nisi, eget egestas nisl nisi euismod nisi. Nullam euismod, nisi vel consectetur interdum, nisl nisi tincidunt nisi, eget egestas nisl nisi euismod nisi. Nullam euismod, nisi vel consectetur interdum, nisl nisi tincidunt nisi, eget egestas nisl nisi euismod nisi. Nullam euismod, nisi vel consectetur interdum,
+        </p>
+      </div>
+      
     </div>
   )
 }
