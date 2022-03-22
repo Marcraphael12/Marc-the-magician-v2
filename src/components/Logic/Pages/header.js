09abae675e2258a670ea1e8e405273f4994c9232
Marc raphael <marcraphael979@gmail.com>
2022-03-22 14:10:33 +0100

    Create the header component

---
+++ src/components/Logic/Pages/header.js
@@ -0,0 +1,23 @@
+const Header = () => {
+  return (
+    <div>
+      <h1>Math Magicians</h1>
+
+      <nav>
+        <ul>
+          <li>
+            <a>Home</a>
+          </li>
+
+          <li>
+            <a>Calculator</a>
+          </li>
+
+          <li>
+            <a>Quote</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
