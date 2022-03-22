1f5b931d39b6b2e4b5cab401adaef490541234e9
Marc raphael <marcraphael979@gmail.com>
2022-03-22 14:16:39 +0100

    Create the 'Quote' component

---
+++ src/components/Logic/Pages/quote.js
@@ -0,0 +1,9 @@
+const Quote = () => {
+	return (
+		<div>
+			<h2>
+				Mathematics is not about numbers, equations, computations, or algorithms: It's about understanding - William Paul Thurston
+			</h2>
+		</div>
+	)
+}
